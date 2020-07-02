f589b1de7bccb120ff722ab753f72d408d81980f
fauntleroy <the.kaempfer@gmail.com>
2020-07-01 23:15:59 -0700

    a more serious attempt at the download page

--- docs/scripts/download.js
+++ docs/scripts/download.js
@@ -1,10 +1,23 @@
-const instructionsCarousel = new Carousel('#instructions-carousel', {
+function operatingSytem () {  
+  let os;
+  if (navigator.appVersion.indexOf('Win') !== -1) os = 'windows'; 
+  if (navigator.appVersion.indexOf('Mac') !== -1) os = 'osx'; 
+  if (navigator.appVersion.indexOf('FreeBSD') !== -1) os = 'free-bsd'; 
+  if (navigator.appVersion.indexOf('Linux') !== -1) os = 'linux'; 
+    
+  return os;
+}  
+
+const defaultInstruction = operatingSytem() || 'source';
+
+function setActiveInstruction (os) {
+  $('.instruction').hide();
+  $('#' + os).show();
+}
+
+$('input[name="operating-system"]').on('change', function (event) {
+  setActiveInstruction(event.target.value);
 });
 
-console.log('instructionsCarousel', instructionsCarousel);
-
-const osOrder = ['windows', 'osx', 'linux', 'free-bsd', 'source'];
-
-$('input[name="operating-system"]').on('change', function (event, other) {
-  instructionsCarousel.goToIndex(osOrder.indexOf(event.target.value));
-});
+setActiveInstruction(defaultInstruction);
+document.querySelector('.download__os-' + defaultInstruction).checked = true;
