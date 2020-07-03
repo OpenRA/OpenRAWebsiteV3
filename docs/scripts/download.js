2d3c56e98df4c98283c56e382855a74186468eaf
fauntleroy <the.kaempfer@gmail.com>
2020-07-02 20:52:27 -0700

    get that download page looking real normal

--- docs/scripts/download.js
+++ docs/scripts/download.js
@@ -1,23 +1,25 @@
-function operatingSytem () {  
-  let os;
-  if (navigator.appVersion.indexOf('Win') !== -1) os = 'windows'; 
-  if (navigator.appVersion.indexOf('Mac') !== -1) os = 'osx'; 
-  if (navigator.appVersion.indexOf('FreeBSD') !== -1) os = 'free-bsd'; 
-  if (navigator.appVersion.indexOf('Linux') !== -1) os = 'linux'; 
-    
-  return os;
-}  
+(function () {
+  function operatingSytem () {  
+    let os;
+    if (navigator.appVersion.indexOf('Win') !== -1) os = 'windows'; 
+    if (navigator.appVersion.indexOf('Mac') !== -1) os = 'osx'; 
+    if (navigator.appVersion.indexOf('FreeBSD') !== -1) os = 'free-bsd'; 
+    if (navigator.appVersion.indexOf('Linux') !== -1) os = 'linux'; 
+      
+    return os;
+  }  
 
-const defaultInstruction = operatingSytem() || 'source';
+  const defaultInstruction = operatingSytem() || 'source';
 
-function setActiveInstruction (os) {
-  $('.instruction').hide();
-  $('#' + os).show();
-}
+  function setActiveInstruction (os) {
+    $('.instruction').hide();
+    $('#' + os).show();
+  }
 
-$('input[name="operating-system"]').on('change', function (event) {
-  setActiveInstruction(event.target.value);
-});
+  $('input[name="operating-system"]').on('change', function (event) {
+    setActiveInstruction(event.target.value);
+  });
 
-setActiveInstruction(defaultInstruction);
-document.querySelector('.download__os-' + defaultInstruction).checked = true;
+  setActiveInstruction(defaultInstruction);
+  document.querySelector('.download__os-' + defaultInstruction).checked = true;
+})();
