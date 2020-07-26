d47aeba1cbfb1cf04e8c0b6fea6e4c65d28f7b69
fauntleroy <the.kaempfer@gmail.com>
2020-07-26 13:52:01 -0700

    complete download button on home page

--- docs/scripts/download.js
+++ docs/scripts/download.js
@@ -1,19 +1,36 @@
 (function () {
+  function getOperatingSystemFromHash (hash) {
+    switch (hash) {
+      case '#windows':
+        return 'windows';
+      case '#osx':
+        return 'osx';
+      case '#free-bsd':
+        return 'free-bsd';
+      case '#linux':
+        return 'linux';
+      case '#source':
+        return 'source';
+      default:
+        return null;
+    }
+  }
+
   function operatingSytem () {  
     let os;
     if (navigator.appVersion.indexOf('Win') !== -1) os = 'windows'; 
     if (navigator.appVersion.indexOf('Mac') !== -1) os = 'osx'; 
     if (navigator.appVersion.indexOf('FreeBSD') !== -1) os = 'free-bsd'; 
     if (navigator.appVersion.indexOf('Linux') !== -1) os = 'linux'; 
-      
+
     return os;
   }  
 
-  const defaultInstruction = operatingSytem() || 'source';
+  const defaultInstruction = getOperatingSystemFromHash(window.location.hash) || operatingSytem() || 'source';
 
   function setActiveInstruction (os) {
     $('.instruction').hide();
-    $('#' + os).show();
+    $('#' + os +'-instructions').show();
   }
 
   $('input[name="operating-system"]').on('change', function (event) {
